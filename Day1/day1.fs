12d347798a6a9449d32928698a8b55ee864aa69d
Ryan Huard <ryan.huard@live.ca>
2018-11-30 19:00:18 -0800

    Create day1 file, learn how to order files

---
+++ Day1/day1.fs
@@ -0,0 +1,8 @@
+// Learn more about F# at http://fsharp.org
+namespace AdventOfCode
+
+open System
+
+module Day1 =
+        let main (argv) = 
+            printfn "Hello from day %i" argv
--- Program.fs
+++ Program.fs
@@ -1,8 +1,10 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open AdventOfCode
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+    Day1.main(1)
     0 // return an integer exit code
