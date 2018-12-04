9953a017a70f7804c53340070d833f64a7bbc276
Ryan Huard <ryan.huard@live.ca>
2018-12-03 20:16:09 -0800

    input and bad start to day3

--- Day3/day3.fs
+++ Day3/day3.fs
@@ -3,6 +3,7 @@ namespace AdventOfCode
 
 open System
 open System.IO
+open System.Text.RegularExpressions
 
 module Day1 =
     let readLines (filePath:string) = seq {
@@ -11,8 +12,30 @@ module Day1 =
             yield sr.ReadLine()
     }
 
+    type claim = {
+    id:int32; 
+    left:int32; 
+    top:int32; 
+    width:int32; 
+    height:int32}
+    let (|Regex|_|) pattern input =
+        let m = Regex.Match(input, pattern)
+        if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+        else None
+    let claimFromString claimStr = 
+        match claimStr with
+        | Regex @"#(\d+) @ (\d+),(\d+): (\d)+x(\d)+" [id; left; top; width; height] ->
+            {id=Int32.Parse(id); left=Int32.Parse(left); top=Int32.Parse(top); width=Int32.Parse(width); height=Int32.Parse(height);}
+
+    let getOverlaps claims =
+        let (claimsArray : int[,]) = Array2D.zeroCreate (1000) (1000)
+
+        0
     let main (argv) = 
-        let nums = "day3\\input.txt" |> readLines |> Seq.map System.Int32.Parse
+        let claims = "day3\\input.txt" |> readLines |> Seq.map claimFromString
+
+
+        
         0
 
 
