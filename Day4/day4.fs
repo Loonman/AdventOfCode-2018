b316109aed4db5cb012a3856e33c410ddeb9ced0
Ryan Huard <ryan.huard@live.ca>
2018-12-03 20:26:19 -0800

    Add day4 placeholder

---
+++ Day4/day4.fs
@@ -0,0 +1,21 @@
+// Learn more about F# at http://fsharp.org
+namespace AdventOfCode
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+module Day1 =
+    let readLines (filePath:string) = seq {
+        use sr = new StreamReader(filePath)
+        while not sr.EndOfStream do
+            yield sr.ReadLine()
+    }
+
+    let main (argv) = 
+        let claims = "day4\\input.txt" |> readLines
+s
+        0
+
+
+
