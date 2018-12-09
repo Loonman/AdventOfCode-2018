14efbef842d339eb52702d6c7d3d0588d2a26126
Ryan Huard <ryan.huard@live.ca>
2018-12-08 22:05:12 -0800

    So lazy

--- Day9/day9.fs
+++ Day9/day9.fs
@@ -4,6 +4,7 @@ namespace AdventOfCode
 open System
 open System.IO
 open System.Text.RegularExpressions
+open System.Collections.Generic
 
 module Day9 =
     let readLines (filePath:string) = seq {
@@ -12,13 +13,15 @@ module Day9 =
             yield sr.ReadLine()
     }
 
-        node
+    let runGame (players:int) (finalMarble:int) = 
+        let (scores : int[]) = Array.zeroCreate(players)
+        let mutable (gameState:List<int>) = new List<int>()
+
+        scores |> Array.toSeq |> Seq.max
+        
     let main (argv) = 
         let players = 258
         let lastMarble = 71307
-        let (scores : int[]) = Array.zeroCreate(players)
+        
         
         0
-
-
-
